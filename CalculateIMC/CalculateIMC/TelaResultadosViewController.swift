83ed474dc4c934503e2d381f79cfb7147645b102
brenopolanski <breno.polanski@gmail.com>
2018-05-26 18:56:37 -0300

    Add references for Labels, Text Fields and Buttons

---
+++ CalculateIMC/CalculateIMC/TelaResultadosViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TelaResultadosViewController.swift
+//  CalculateIMC
+//
+//  Created by user140218 on 5/26/18.
+//  Copyright © 2018 user140218. All rights reserved.
+//
+
+import UIKit
+
+class TelaResultadosViewController: UIViewController {
+
+    @IBOutlet weak var labelNomeCompleto: UILabel!
+    @IBOutlet weak var labelValorIdade: UILabel!
+    @IBOutlet weak var labelResultado: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CalculateIMC/CalculateIMC/ViewController.swift
+++ CalculateIMC/CalculateIMC/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var valorImc: Float = 0.0;
+    var nomeCompleto: NSString = "";
+    var idade: Float = 0.0;
+    
+    @IBOutlet weak var textFieldNomeCompleto: UITextField!
+    @IBOutlet weak var textFieldAltura: UITextField!
+    @IBOutlet weak var textFieldPeso: UITextField!
+    @IBOutlet weak var textFieldIdade: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func limparCampos(_ sender: Any) {
+        textFieldNomeCompleto.text = nil;
+        textFieldAltura.text = nil;
+        textFieldPeso.text = nil;
+        textFieldIdade = nil;
+    }
 }
 
