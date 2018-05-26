03e33161f13e38b67f9803dbae4b240016e7b3d1
brenopolanski <breno.polanski@gmail.com>
2018-05-26 19:42:15 -0300

    Add functions

--- CalculateIMC/CalculateIMC/ViewController.swift
+++ CalculateIMC/CalculateIMC/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var valorImc: Float = 0.0;
-    var nomeCompleto: NSString = "";
-    var idade: Float = 0.0;
+    var valorImc: Float = 0.0
+    var nomeCompleto: NSString = ""
+    var idade: Float = 0.0
     
     @IBOutlet weak var textFieldNomeCompleto: UITextField!
     @IBOutlet weak var textFieldAltura: UITextField!
@@ -28,12 +28,35 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func calculaImc(_ sender: Any) {
+        guard let sNomeCompleto = self.textFieldNomeCompleto.text,
+        self.textFieldNomeCompleto.text?.isEmpty==false else {
+            exibirAlerta()
+            return
+        }
 
+        guard let sAltura = self.textFieldAltura.text,
+        self.textFieldAltura.text?.isEmpty==false else {
+            exibirAlerta()
+            return
+        }
+    }
+    
     @IBAction func limparCampos(_ sender: Any) {
-        textFieldNomeCompleto.text = nil;
-        textFieldAltura.text = nil;
-        textFieldPeso.text = nil;
-        textFieldIdade = nil;
+        textFieldNomeCompleto.text = nil
+        textFieldAltura.text = nil
+        textFieldPeso.text = nil
+        textFieldIdade.text = nil
+    }
+    
+    func exibirAlerta() {
+        let alerta = UIAlertController(title: "Alerta", message: "Preencha todos os campos", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+        
+        alerta.addAction(action)
+        
+        present(alerta, animated: true, completion: nil)
     }
 }
-
