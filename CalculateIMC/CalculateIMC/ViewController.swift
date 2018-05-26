932a41487102a82026ed0675aa32ad398f661ab5
brenopolanski <breno.polanski@gmail.com>
2018-05-26 15:48:19 -0300

    Initial code

---
+++ CalculateIMC/CalculateIMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculateIMC
+//
+//  Created by user140218 on 5/26/18.
+//  Copyright © 2018 user140218. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CalculateIMC/CalculateIMCTests/CalculateIMCTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalculateIMCTests.swift
+//  CalculateIMCTests
+//
+//  Created by user140218 on 5/26/18.
+//  Copyright © 2018 user140218. All rights reserved.
+//
+
+import XCTest
+@testable import CalculateIMC
+
+class CalculateIMCTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
